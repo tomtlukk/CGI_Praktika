5c733075260d3c359e690ef6bee273f6a4ff6d88
tomtlukk <tomtimo@ut.ee>
2026-03-12 20:10:47 +0200

    Initial commit

---
+++ src/test/java/com/example/cgi_praktika/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cgi_praktika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
