eab739e2752d0c0cf7d6f494c52c9bcb2d36de63
tomtlukk <tomtimo@ut.ee>
2026-03-20 02:10:55 +0200

    Create H2 database schema

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,21 @@
+/*DROP TABLE IF EXISTS restaurant_tables; */
+
+CREATE TABLE IF NOT EXISTS restaurant_tables (
+    tableId INTEGER AUTO_INCREMENT,
+    tableCapacity INTEGER,
+    tableZone VARCHAR(255),
+    tablePreferences VARCHAR(255),
+    startingPosX INTEGER,
+    startingPosY INTEGER,
+    tableWidth INTEGER,
+    PRIMARY KEY (tableId)
+);
+/* DROP TABLE IF EXISTS reservations; */
+
+CREATE TABLE IF NOT EXISTS reservations (
+    reservationId INTEGER AUTO_INCREMENT,
+    tableId INTEGER,
+    reservationFrom TIMESTAMP,
+    reservationUntil TIMESTAMP,
+    primary key (tableId)
+    );
