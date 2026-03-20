11d90522879e85b42cd7188edc85cb22045b986f
tomtlukk <tomtimo@ut.ee>
2026-03-20 11:58:05 +0200

    Update Reservation record with validation

--- src/main/java/com/example/cgi_praktika/model/Reservation.java
+++ src/main/java/com/example/cgi_praktika/model/Reservation.java
@@ -1,11 +1,18 @@
 package com.example.cgi_praktika.model;
 
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDateTime;
 
 public record Reservation(
+        @NotNull
         Integer reservationId,
+        @NotNull
         Integer tableId,
+        @FutureOrPresent
         LocalDateTime reservationFrom,
+        @FutureOrPresent
         LocalDateTime reservationUntil
         ) {
 }
